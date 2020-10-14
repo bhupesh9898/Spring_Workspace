92c0e6f0f22da6fd48740b14614bef5bd6d218ef
GouravKaushal <gouravkaushal11111.gk@gmail.com>
2020-10-14 22:09:52 +0530

    14/10/2020

--- Employee_Web_Service/src/main/java/com/zensar/EmployeeWebServiceApplication.java
+++ Employee_Web_Service/src/main/java/com/zensar/EmployeeWebServiceApplication.java
@@ -3,7 +3,7 @@ package com.zensar;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class EmployeeWebServiceApplication {
 
 	public static void main(String[] args) {
