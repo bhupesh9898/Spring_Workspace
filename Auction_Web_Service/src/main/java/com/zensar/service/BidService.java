b6b3690bc9ed71810e9fd8a2793ff618b678c35f
GouravKaushal <gouravkaushal11111.gk@gmail.com>
2020-10-18 17:21:53 +0530

    test

--- Auction_Web_Service/src/main/java/com/zensar/service/BidService.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.service;
-
-import com.zensar.model.Bid;
-
-public interface BidService {
-	 Iterable<Bid> bidHistory(int productId);
-	 int placeBid (Bid bid);
-	 Iterable<Bid> getBidsByBidderId(int bidderId);
-}
