9321e6bcb1ae28472181bfc8e3a680a5d987de2a
bhupesh9898 <59949400+bhupesh9898@users.noreply.github.com>
2021-01-15 22:07:07 +0530

    Delete Auction_Web_Service

--- Auction_Web_Service/src/main/java/com/zensar/service/ProductCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.service;
-
-import com.zensar.model.ProductCategory;
-
-public interface ProductCategoryService {
-	Iterable<ProductCategory> getProductCategories();
-	int addProductCategory(ProductCategory productCategory);
-	ProductCategory getProductCategoryByName(String productCategoryName);
-}
